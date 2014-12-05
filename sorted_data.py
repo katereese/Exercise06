508f6b0f999a03e87bbdfa2bb8c62785cfeb91c8
kreese <kate.leigh.reese@gmail.com>
2014-12-04 21:33:30 -0800

    adding readme file.

--- sorted_data.py
+++ sorted_data.py
@@ -9,22 +9,33 @@ from sys import argv
 
 script, filename = argv
 
-opened_file = open(filename)
+def read_file(filename):
+	opened_file = open(filename)
+	rest_dict = {}
 
-# dictionary = {}
+	for line in opened_file:
+		line = line.rstrip()
+		words = line.split(":")
+		restaurant_name = words[0]
+		restaurant_score = words[1]
 
-for line in opened_file:
-    words = line.split(":")
-    restaurant_name = words[0]
-    restaurant_score = words[1]
-    print restaurant_name restaurant_score
-    
-    # for name, score in zip(restaurant_name, restaurant_score):
-        # print "Restaurant {0} is rated at {1}.".format(name, score)
+		rest_dict[restaurant_name] = restaurant_score
 
+	for restaurant_name in sorted (rest_dict):
+		print "Restaurant %s is rated at %s." % (restaurant_name, rest_dict[restaurant_name])
+		
+# print str(sorted(rest_list))
+# print "Restaurant {0} is rated at {1}.".format(name, score)
 
-# print dictionary
+def print_ratings(rest_dict):
 
+	for i in sorted(rest_dict.keys()):
+	   print 'Restaurant %s is rated %s.' % (i, rest_dict[i])
 
+def main():
 
+	rest_dict = read_file(filename)
+	print_ratings(rest_dict)
 
+if __name__ == '__main__':
+	main()
